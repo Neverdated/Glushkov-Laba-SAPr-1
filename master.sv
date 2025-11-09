fb087477ad3a65aa16398536759144f18e45994f
Neverdated <mort_dangerous@mail.ru>
2025-11-09 20:58:03 +0300

    Модифицирован модуль TB. Теперь он записывает все нужные данные в память.

--- master.sv
+++ master.sv
@@ -102,14 +102,83 @@ begin
 	repeat (5) @(posedge pclk);
 
 	address = p_device_offset+0;
-	data_to_device = 32'h12345678;
+	data_to_device = 32'd6;
 	apb_write(address, data_to_device);
 	apb_read(address, data_from_device);
-	$display("Addr= 0x%h, write data 0x%h, read data 0x%h", address, data_to_device, data_from_device);
-	data_to_device = 32'h1;
+	$display("Addr= 0x%h, write data 0x%d, read data 0x%d", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'h4;
+	data_to_device = 32'd9;
 	apb_write(address, data_to_device);
 	apb_read(address, data_from_device);
-	$display("Addr= 0x%h, write data 0x%h, read data 0x%h", address, data_to_device, data_from_device);
+	$display("Addr= 0x%h, write data 0x%d, read data 0x%d", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'h5;
+	data_to_device = 32'd11;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%d, read data 0x%d", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'h6;
+	data_to_device = 32'd20;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%d, read data 0x%d", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'h7;
+	data_to_device = 32'd25;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%d, read data 0x%d", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'h8;
+	data_to_device = 32'h47;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'h9;
+	data_to_device = 32'h4c;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'ha;
+	data_to_device = 32'h55;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'hb;
+	data_to_device = 32'h53;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'hc;
+	data_to_device = 32'h41;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'hd;
+	data_to_device = 32'h50;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'he;
+	data_to_device = 32'h48;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+	
+	address = p_device_offset+8'hf;
+	data_to_device = 32'h41;
+	apb_write(address, data_to_device);
+	apb_read(address, data_from_device);
+	$display("Addr= 0x%h, write data 0x%c, read data 0x%c", address, data_to_device, data_from_device);
+
 	repeat (10) @(posedge pclk);
 	$stop();
 
